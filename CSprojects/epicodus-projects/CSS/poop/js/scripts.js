52b4fce5ef2fa76253158e5c1f4d93524ac784a4
Gracie Lundell <gracielundell@gmail.com>
2016-03-07 10:08:14 -0800

    add css files

---
+++ CSprojects/epicodus-projects/CSS/poop/js/scripts.js
@@ -0,0 +1,53 @@
+var squareImg = "red";
+
+var circleArray = [
+  {
+    imgSource: "carrot.png"
+  },
+  {
+    imgSource: "flower.png"
+  },
+  {
+    imgSource: "tomato.png"
+  },
+  {
+    imgSource: "basil.png"
+  },
+  {
+    imgSource: "potatoes.png"
+  },
+  {
+    imgSource: "garlic.jpg"
+  },
+  {
+    imgSource: "dirt.png"
+  }
+];
+
+// set img for circles
+function createCircleDiv(circle) {
+  return "<div class='circle' style='background-image:" + "url(\"img/" + circle.imgSource + "\")'></div>"
+};
+
+// create circles
+$(document).ready(function() {
+  circleArray.forEach(function(circle) {
+    var circleDiv = createCircleDiv(circle);
+    $(".circles").append(circleDiv);
+  });
+
+  // set square's background img
+  $(".wrapper").click(function(event) {
+    $(event.target).css("background-image", squareImg);
+  });
+
+  // set squareImg to circle's img
+  $(".circle").click(function(event) {
+    // remove selector when new circle is clicked on
+    $('.circle').removeClass("selected")
+
+    squareImg = $(event.target).css("background-image");
+    $(event.target).addClass("selected");
+  });
+
+});
