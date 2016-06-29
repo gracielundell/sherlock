ba9ad2a2b1416fa5d325897b000e602245f6f72e
Gracie Lundell <gracielundell@gmail.com>
2016-06-28 22:04:52 -0700

    add hover mixin to change column size when that column is being hovered over

---
+++ CSprojects/practice/javascript/transitions/js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//   console.log("outside if");
+//   $(".dimensions").click(function() {
+//     console.log(this);
+//     console.log("i got into the if");
+//     if($(this).hasClass("open")) {
+//       $(this).removeClass("open");
+//     } else {
+//       $(this).addClass("open");
+//     }
+//   });
+// });
