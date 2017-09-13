92899f6a7b0c1775d35ca9aef5a914e4155a1836
Gracie Lundell <gracielundell@gmail.com>
2017-09-12 20:57:11 -0700

    access value of title and description in the console log

---
+++ CSprojects/vue-projects/dreamSpeak/app.js
@@ -0,0 +1,11 @@
+// var inputContainer = document.getElementsByClassName('inputContainer');
+// var outputContainer = document.getElementsByClassName('outputContainer');
+// var submitPost = document.getElementsByClassName('submitPost'); //submit button
+
+
+function showPost() {
+  var title = document.getElementById("entryTitle").value;
+  var description = document.getElementById("entryDescription").value;
+  console.log(title);
+  console.log(description);
+}
